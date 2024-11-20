8c0af8eed39e5ec80a540b1e5dec4c957cf5d846
bouaichamohamedanouer <119801389+bouaichamohamedanouer@users.noreply.github.com>
2024-11-20 16:49:04 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -26,7 +26,9 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
-  
+  @app.route('/')
+def hello_world():
+    return render_template('histogramme.html') #comment3
 
 
 
