d5c9d855e43e81ef269b3b48e066f36a5e2c54ff
bouaichamohamedanouer <119801389+bouaichamohamedanouer@users.noreply.github.com>
2024-11-20 17:21:51 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,14 +6,15 @@ from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)   
+  
+@app.route('/')
+def hello_world():
+    return render_template('hello.html') #comment2  
 
 @app.route("/contact/")
 def contact():
     return render_template("contact.html")
   
-@app.route('/')
-def hello_world():
-    return render_template('hello.html') #comment2  
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
@@ -25,6 +26,7 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)  
+  
   @app.route("/commits/")
 def commits():
     # URL de l'API GitHub pour les commits
